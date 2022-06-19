a635f6c3fa1855e090b03fbd0c86cd576cc9142b
nahidul77 <nahidul.mee@gmail.com>
2022-06-19 16:05:50 +0600

    magic methods

---
+++ 01 Basics/05 OOP/14_magic_methods.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Automatically run based on condition
+ * such as __construct
+ * They start with prefix (__) function
+ * 
+ * Magic Method List
+ * 
+ * __construct()
+ * __destruct()
+ * __get()
+ * __set()
+ * __isset()
+ * __unset()
+ * __autoload()
+ * __clone()
+ * __sleep()
+ * __wakeup()
+ * __call()
+ * __callStatic()
+ * __toString()
+ * __invoke()
+ */
