d71e32fe9e3eb5ba915ded973c37f47d9b8f0de6
nahidul77 <nahidul.mee@gmail.com>
2022-06-27 18:37:11 +0600

    for loop and exit

---
+++ PHP5_BIBLE/05_Chapter/5_control_and_function.php
@@ -0,0 +1,21 @@
+<?php
+
+//________________________________For loop_______________
+
+/**
+ * It is also legal to include more than one of each kind of for clause, separated by commas.
+The termination-check will be considered to be true if any of its subclauses are true; it is like
+an ‘or’ test. For example, the following statement:
+ */
+// for (
+//     $x = 1, $y = 1, $z = 1; //initial expressions
+//     $y < 10, $z < 10; // termination checks
+//     $x = $x + 1, $y = $y + 2, // loop-end expressions
+//     $z = $z + 3
+// )
+// print("$x, $y, $z \n");
+
+/**
+ * The exit() construct takes either a string or a number as argument, prints out the argument,
+and then terminates execution of the script. die() is a alias of exit()
+ */
