f6e6594563b15ef1e84fb303a183f909396365c3
nahidul77 <nahidul.mee@gmail.com>
2022-06-28 10:36:21 +0600

    http

---
+++ PHP5_BIBLE/05_Chapter/7_passing_information_between_pages.php
@@ -0,0 +1,12 @@
+<?php
+
+//______________________Http__________________
+
+/**
+ * HTTP protocol itself is stateless. If you are a poetic soul, you might say that each HTTP request is on its own, with no direction home, like a complete unknown.
+ * 
+ * each HTTP request —
+in most cases, this translates to each resource (HTML page, .jpg file,style sheet, and so on) being asked for and delivered — is independent of all the others, knows nothing substantive about the identity of the client, and has no memory.
+
+
+ */
