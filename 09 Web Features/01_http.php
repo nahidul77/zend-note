7fe33c295df88d9e7ff05bfe6587c808e0bd1dc1
nahidul77 <nahidul.mee@gmail.com>
2022-06-30 10:54:41 +0600

    web feature http res and req

---
+++ 09 Web Features/01_http.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * General___________________
+ * 
+ * Request Url
+ * Request Method
+ * Status Code
+ * Remote Address (ip of a remote computer)
+ * Refferrer Policy (information about going one page to another)
+ * 
+ * Response___________________
+ * 
+ * Server (Mostly hidden so that dont know what kind of server using)
+ * set cookie (set small piece of data called cookies send server to client)
+ * content-type (text/html, text/css, image/png, application/json)
+ * content-length
+ * date
+ * 
+ * Request____________________
+ * 
+ * Cookies
+ * accept-xxx (accept-coding, accept-charset)
+ * content-type
+ * content-length
+ * Authorization (token)
+ * User-agent (operationg system, browser)
+ * Referrer (referrer site)
+ * 
+ * http status code_________________________________
+ * 
+ * 
+ */
