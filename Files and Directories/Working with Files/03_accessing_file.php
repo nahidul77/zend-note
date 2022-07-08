6ee16ecda4427945b9c1d3aff6e580dbb5b55827
nahidul77 <nahidul.mee@gmail.com>
2022-07-09 01:14:21 +0600

    file accessing

---
+++ Files and Directories/Working with Files/03_accessing_file.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * fopen(filepath, mode), return a file handle or file reference which file we will working on.
+ * 
+ * r (Read from start, must exist)
+ * w (Truncate/write from start)
+ * a (Append/ write from end)
+ * 
+ * recommendation
+ * 
+ * w: strating a new file
+ * r+: reading/writing to an existing file
+ * r: read only access to a file
+ * a: appending data to the end of a file
+ */
