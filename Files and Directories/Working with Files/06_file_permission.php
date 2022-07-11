fef4f134389156315ec89f31d4527bbf075a3b9f
nahidul77 <nahidul.mee@gmail.com>
2022-07-11 10:51:35 +0600

    file permission

---
+++ Files and Directories/Working with Files/06_file_permission.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * File Permissions
+ * 
+ * Files and directories belogn to a user
+ * web server is often a different user
+ * Permission determine who can access files
+ * cannot read or write a file without file permissions
+ * cannot create or delete a file without directory permissions
+ * Setting permissions varies by operating system
+ * Windows GUI: Properties > Security > Edit
+ * Unix: chmod 
+ * rwx(user)rwx(group)rwx(other)
+ */
