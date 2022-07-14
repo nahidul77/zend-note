6540020390c5fd35d2d2abf4a0cc0deb670e8487
nahidul77 <nahidul.mee@gmail.com>
2022-07-14 12:22:55 +0600

    file writes

---
+++ Files and Directories/Working with Files/07_writing_to_files.php
@@ -0,0 +1,21 @@
+<?php
+
+$filepath = __DIR__ . '/sample_file.txt';
+
+$handle = fopen($filepath, 'w'); // overwrite
+if($handle) {
+  fwrite($handle, 'abcdef');
+  fwrite($handle, '1234567890');
+  fclose($handle);
+} else {
+  echo "Could not open file.";
+}
+
+$filepath = __DIR__ . '/sample_file2.txt';
+
+// Unix/Mac new lines are "\n"
+// Windows new lines are "\r\n"
+// double-quotes matter
+$data = "A page of data\n111\n222\n333\n";
+
+file_put_contents($filepath, $data);
