7eb43751a6148eeac7e595b9eb755e3abf328a0e
nahidul77 <nahidul.mee@gmail.com>
2022-07-20 17:09:27 +0600

    file system funcitons

--- Files and Directories/working with directories/05_treasure_hunt.php
+++ Files and Directories/working with directories/05_treasure_hunt.php
@@ -1,37 +1,41 @@
 <?php
 
-function walk_directory($dirpath) {
-  $handle = opendir($dirpath);
-  if($handle) {
-    while(false !== ($entry = readdir($handle))) {
-      // skip all . files
-      if(stripos($entry, '.') === 0) { continue; }
+function walk_directory($dirpath)
+{
+    $handle = opendir($dirpath);
+    if ($handle) {
+        while (false !== ($entry = readdir($handle))) {
+            // skip all . files
+            if (stripos($entry, '.') === 0) {
+                continue;
+            }
 
-      $entry_path = $dirpath . '/' . $entry;
-      if(is_dir($entry_path)) {
-        walk_directory($entry_path);
-      } else {
-        examine_file($entry_path);
-      }
+            $entry_path = $dirpath . '/' . $entry;
+            if (is_dir($entry_path)) {
+                walk_directory($entry_path);
+            } else {
+                examine_file($entry_path);
+            }
+        }
+        closedir($handle);
+    } else {
+        echo "Could not open directory.";
     }
-    closedir($handle);
-  } else {
-    echo "Could not open directory.";
-  }
 }
 
-function examine_file($filepath) {
-  // echo "Looking in {$filepath}<br />";
-  $handle = fopen($filepath, 'r');
-  if($handle) {
-    $data = fread($handle, 8); // treasure = 8 bytes/characters
-    if($data == 'treasure') {
-      echo "Found treasure! {$filepath}<br />";
+function examine_file($filepath)
+{
+    // echo "Looking in {$filepath}<br />";
+    $handle = fopen($filepath, 'r');
+    if ($handle) {
+        $data = fread($handle, 8); // treasure = 8 bytes/characters
+        if ($data == 'treasure') {
+            echo "Found treasure! {$filepath} \n \n";
+        }
+        fclose($handle);
+    } else {
+        echo "Could not open file.";
     }
-    fclose($handle);
-  } else {
-    echo "Could not open file.";
-  }
 }
 
 $huntpath = __DIR__ . '/assets/treasure_hunt';
