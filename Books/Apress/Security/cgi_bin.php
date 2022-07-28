472d5a833aea3b2d00e92b0ee517760582936731
nahidul77 <nahidul.mee@gmail.com>
2022-07-28 21:28:55 +0600

    cgi, xml,

---
+++ Books/Apress/Security/cgi_bin.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * 
+ * Common Gateway Interface
+ * 
+ * is a set of rules for running script and programms on a web server
+ * 
+ * It specifies what information is communicated between web server and client's web browser and how information is transmitted. 
+ * 
+ * Most web server include a cgi-bin directory in the root folder of each website on the server.
+ * 
+ * Any Script placed in this directory must follow the rules of common gateway interface
+ * 
+ * Since CGI is a standard interface, it can be used on multiple types of hardware platforms and is supported by several types web server software such as Apache and windows server.
+ *  
+ * CGI programme is executed in real time , so that it can output dynamic information. 
+ * 
+ * CGI Programme can be used to access other resources like database., graphical packages
+ * 
+
+
