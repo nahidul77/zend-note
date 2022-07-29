df270e0da7dbb39ac471e03df4da2f072e1bcde0
nahidul77 <nahidul.mee@gmail.com>
2022-07-29 23:04:59 +0600

    zend practice book, Q7

--- Books/Zend Practice Book/01_basic.php
+++ Books/Zend Practice Book/01_basic.php
@@ -15,14 +15,41 @@
 
 #Q6
 
-$a = 10;
-$b = 20;
-$c = 4;
-$d = 8;
-$e = 1.0;
-$f = $c + $d * 2;
-$g = $f % 20;
-$h = $b - $a + $c + 2;
-$i = $h << $c;
-$j = $i * $e;
-print $j; //output : 256
+// $a = 10;
+// $b = 20;
+// $c = 4;
+// $d = 8;
+// $e = 1.0;
+// $f = $c + $d * 2;
+// $g = $f % 20;
+// $h = $b - $a + $c + 2;
+// $i = $h << $c;
+// $j = $i * $e;
+// print $j; //output : 256
+
+
+#Q7
+
+// $string = "Hello, World!";
+// $a = false;
+// $b = false;
+// $c = true;
+// if ($a) {
+//     if ($b && !$c) {
+//         echo "Goodbye Cruel World!";
+//     } else if (!$b && !$c) {
+//         echo "Nothing here";
+//     }
+// } else {
+//     if (!$b) {
+//         if (!$a && (!$b && $c)) {
+//             echo "Hello, World!";
+//         } else {
+//             echo "Goodbye World!";
+//         }
+//     } else {
+//         echo "Not quite.";
+//     }
+// }
+
+// Output: Hello, World!;
