36a581110e69360636610d1595e9bd7319baa6d9
nahidul77 <nahidul.mee@gmail.com>
2022-07-29 22:59:35 +0600

    zend practice book, Q6

--- Books/Zend Practice Book/01_basic.php
+++ Books/Zend Practice Book/01_basic.php
@@ -11,3 +11,18 @@
 // print $myarray[myvalue] . "\n";
 
 // Output: The value is: Dog
+
+
+#Q6
+
+$a = 10;
+$b = 20;
+$c = 4;
+$d = 8;
+$e = 1.0;
+$f = $c + $d * 2;
+$g = $f % 20;
+$h = $b - $a + $c + 2;
+$i = $h << $c;
+$j = $i * $e;
+print $j; //output : 256
