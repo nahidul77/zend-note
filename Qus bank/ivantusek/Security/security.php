8664d4906ab6827a7e515fb5cd1b7187ba3c0d0a
nahidul77 <nahidul.mee@gmail.com>
2022-08-05 01:14:23 +0600

    binary safe

--- Qus bank/ivantusek/Security/security.php
+++ Qus bank/ivantusek/Security/security.php
@@ -54,3 +54,23 @@
 /**
  * The open_basedir configuration directive will limit the files that can be opened by PHP to the specified directory-tree. When a script tries to open a file with, for example, fopen() or gzopen(), the location of the file is checked. When the file is outside the specified directory-tree, PHP will refuse to open it. open_basedir is a good protection against remote file inclusion vulnerabilities. For a remote attacker it is not possible to break out of the open_basedir restrictions if he is only able to inject the name of a file to be included. Therefore the number of files he will be able to include with such a local file include vulnerability is limited.
  */
+
+
+#23 
+
+/**
+ * Binary safe
+
+  In PHP, the meaning is more specific, referring only to what Michael gives as an example.
+
+All strings in PHP have a length associated, which are the number of bytes that compose it. When a function manipulates a string, it can either:
+
+Rely on that length meta-data.
+Rely on the string being null-terminated, i.e., that after the data that is actually part of the string, a byte with value 0 will appear.
+It's also true that all string PHP variables manipulated by the engine are also null-terminated. The problem with functions that rely on 2., is that, if the string itself contains a byte with value 0, the function that's manipulating it will think the string has ended at that point and will ignore everything after that.
+
+For instance, if PHP's strlen function worked like C standard library strlen, the result here would be wrong:
+
+$str = "abc\x00abc";
+echo strlen($str); //gives 7, not 3!
+ */
