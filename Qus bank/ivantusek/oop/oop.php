f14e7332a8889221957b9a7421c6e579736abca8
nahidul77 <nahidul.mee@gmail.com>
2022-08-06 16:35:41 +0600

    object pass by ref

---
+++ Qus bank/ivantusek/oop/oop.php
@@ -0,0 +1,24 @@
+<?php
+#1
+
+
+// $x = array(10, 2, 4);
+// $y = array(1 => 2, 0 => 10, 2 => 4);
+
+// var_dump($x == $y); //bool(true)
+
+
+#10
+
+//objects are passed by references by default
+
+// class test1
+// {
+//     public $name;
+// }
+// $obj1 = new test1();
+// echo "ID of obj1: " . spl_object_hash($obj1) . "\n"; //0000000032f49af8000000006f7614d2
+// $obj2 = $obj1;
+// echo "ID of obj2: " . spl_object_hash($obj2); //0000000032f49af8000000006f7614d2
+// $obj1->name = "Amar";
+// echo "name: " . $obj2->name; //name: Amar
