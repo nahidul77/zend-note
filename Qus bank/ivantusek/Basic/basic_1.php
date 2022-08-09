0648b5d44926227d27a583ca8c54a35b5695a8f2
nahidul77 <nahidul.mee@gmail.com>
2022-08-10 05:29:30 +0600

    ivantusek basic_1 till 14

---
+++ Qus bank/ivantusek/Basic/basic_1.php
@@ -0,0 +1,72 @@
+<?php
+#1
+
+// $a = 0;
+
+// if ($a = 10) {
+//     echo "first";
+// }
+
+// if ($a == 0) {
+//     echo "Second";
+// }
+
+// if ($a == "0") {
+//     echo "Third";
+// }
+
+// first
+
+#08 
+
+// $i = 100;
+// $j = $i++ - 10;
+// echo $i . '-' . $j; // 101-90
+
+#10
+
+// echo 8 / 4 * 4; // left to right -- 8
+
+// echo 4 * 4 / 8; // 2
+
+#13
+
+// $a = ($b = 13) - 5;
+// echo $a . '-' . $b; // 8 - 13
+
+
+#13 
+
+// $a = '1';
+// $b = '01';
+
+// if ($a == $b) {
+//     echo '1 and 01 is equal';
+// }
+
+
+#14
+
+/**
+ * The + operator returns the right-hand array appended to the left-hand array; for keys that exist in both arrays, the elements from the left-hand array will be used, and the matching elements from the right-hand array will be ignored.
+ */
+
+// $a = array('z', 'x');
+// $b = array('y', 'w');
+
+// $e = $a + $b;
+
+// $c = array('a' => 1, 'b' => 2);
+// $d = array('c' => 3, 'd' => 4);
+
+// $f = $c + $d;
+
+// foreach ($e as $value) {
+//     echo $value;
+// }
+
+// foreach ($f as $value) {
+//     echo $value;
+// }
+
+//zx1234
