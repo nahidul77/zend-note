703c8f4c3bad42a1728d7af7b61980acb3f2185e
nahidul77 <nahidul.mee@gmail.com>
2022-08-18 00:43:33 +0600

    Array till 27 qus

--- Qus bank/ivantusek/function & array/array.php
+++ Qus bank/ivantusek/function & array/array.php
@@ -6,3 +6,58 @@
 
 // echo array_search('15', $values); //0
 // echo array_search('15', $values, true); //2
+
+
+#22
+
+// error_reporting(E_ALL | E_STRICT);
+// $newArray[E_STRICT] = 'foo'; //VALID
+
+
+#24
+
+// $arr = array_flip(
+//     array(
+//         'a' => 1,
+//         'b' => 2,
+//         'c' => 3
+//     )
+// );
+
+// print_r($arr);
+
+// Array
+// (
+//     [1] => a
+//     [2] => b
+//     [3] => c
+// )
+
+// $values = array_values($arr);
+
+// print_r($values);
+
+// Array
+// (
+//     [0] => a
+//     [1] => b
+//     [2] => c
+// )
+
+// foreach ($values as $k => $v) {
+//     echo $k;
+// }
+
+
+#27___________________________________
+
+// $numbers = array(5, 6, 7, 8);
+
+// end($numbers);
+
+// while (key($numbers)) {
+//     echo current($numbers);
+//     prev($numbers);
+// }
+
+//876
