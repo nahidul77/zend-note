60a43342a3faa1a6bb202579144d03ff67a05a1a
nahidul77 <nahidul.mee@gmail.com>
2022-09-05 16:52:08 +0600

    freecram and test prep till 20

---
+++ Qus bank/freecram/qus.php
@@ -0,0 +1,30 @@
+<?php
+
+#5 
+
+// $str = array('message' => 'Hello, world!');
+// $str = array('Hello, world!');
+// $str = 'Hello, world!';
+
+// $str = function () {
+//     alert('hello, world!');
+// };  // cannot be encoded
+
+// echo json_encode($str);
+
+
+// $a = 3;
+
+// switch ($a) {
+//     case 1:
+//         echo 'one';
+//         break;
+//     default:
+//         echo 'two';
+//         break;
+//     case 3:
+//         echo 'three';
+//         break;
+// }
+
+// output : three
---
+++ Qus bank/testprep/01_exam.php
@@ -0,0 +1,28 @@
+<?php
+
+## Q2
+
+// class C
+// {
+//     public $x = 1;
+//     function __construct()
+//     {
+//         ++$this->x;
+//     }
+
+//     function __invoke()
+//     {
+//         return ++$this->x;
+//     }
+
+//     function __toString()
+//     {
+//         return (string) --$this->x;
+//     }
+// }
+
+// $obj = new C();
+
+// echo $obj(); // 3
+
+// echo $obj; // 2
