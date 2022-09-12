4cc42efeb6c1bcb48fcf0c22179d8a4bc284759a
nahidul77 <nahidul.mee@gmail.com>
2022-09-12 23:42:53 +0600

    static chaining

---
+++ Courses/MASUD ALOM/13 class/static_chaining.php
@@ -0,0 +1,32 @@
+<?php
+
+class Math
+{
+    public static $val;
+
+    public static function init($var)
+    {
+        static::$val = $var;
+        return new static;
+    }
+
+    public static function add($var)
+    {
+        static::$val += $var;
+        return new static;
+    }
+
+    public static function sub($val)
+    {
+        static::$val -= $val;
+        return new static;
+    }
+
+    public static function result()
+    {
+        return static::$val;
+    }
+}
+
+
+echo Math::init(10)->add(20)->sub(30)->result();
