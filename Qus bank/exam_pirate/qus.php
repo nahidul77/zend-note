52b4da5c51cee96522ece328c66f00659373531d
nahidul77 <nahidul.mee@gmail.com>
2022-09-15 02:05:55 +0600

    exam pirate qus till 46

--- Qus bank/exam_pirate/qus.php
+++ Qus bank/exam_pirate/qus.php
@@ -52,4 +52,50 @@
 //     }
 // }
 
-B::test();  //Base B B A B 
+// B::test();  //Base B B A B 
+
+
+#31_________________________________________
+
+// class A
+// {
+
+//     public $a = 1;
+
+//     public function __construct($a)
+//     {
+//         $this->a = $a;
+//     }
+
+//     public function mul()
+//     {
+
+//         return function ($x) {
+
+//             return $this->a * $x;
+//         };
+//     }
+// }
+
+// $a = new A(2);
+
+// $a->mul = function ($x) {
+
+//     return $x * $x;
+// };
+
+// $m = $a->mul();
+
+
+// echo $m(3);  // 6
+
+
+#40___________________________
+
+// printf('%010.6f', 22); //022.000000
+
+#47__________________________
+
+preg_match('/^(\d{1,2}([a-z]+))(?:\s*)\S+ (?=201[0-9])/', '21st March 2014', $matches);
+
+print_r($matches);
