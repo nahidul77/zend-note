1d890ecbd868561ab3da2f4f01cbb61369e46aa7
nahidul77 <nahidul.mee@gmail.com>
2022-09-29 17:33:09 +0600

    601-700 qus started

--- Qus bank/alceanicu/601_700_qus.php
+++ Qus bank/alceanicu/601_700_qus.php
@@ -1,1 +1,7 @@
 <?php
+$str = "Jane \"&\" 'Tarzan'";
+echo htmlspecialchars($str); // Will only convert double quotes
+echo "\n";
+echo htmlspecialchars($str, ENT_QUOTES); // Converts double and single quotes
+echo "\n";
+echo htmlspecialchars($str, ENT_NOQUOTES); // Does not convert any quotes
