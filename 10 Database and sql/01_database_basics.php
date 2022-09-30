3a5f68d1ba4906565ad41d96acb35f940dde1ec0
nahidul77 <nahidul.mee@gmail.com>
2022-09-30 18:54:19 +0600

    database and sql facts

---
+++ 10 Database and sql/01_database_basics.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * 
+ * Facts
+PHP no longer ships with the MySQL extension, instead MySQLi is included.
+The MySQLi extension makes some of MySQL’s more recent functionality available, things like prepared statements
+There are two types of keys: primary key and foreign key
+int or integer – Signed integer number, 32 bits in length.
+smallint – Signed integer number, 16 bits in length.
+real – Signed floating-point number, 32 bits in length.
+float – Signed floating-point number, 64 bits in length.
+char Fixed-length character string.
+varchar Variable-length character string
+datetime – store date time values (e.g. timestamp)
+BLOB Binary Large OBject
+
+ */
