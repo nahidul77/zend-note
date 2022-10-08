ea86a59db595073d2d2a18398214c7e813847a05
nahidul77 <nahidul.mee@gmail.com>
2022-10-08 18:25:41 +0600

    testprep preg_split

---
+++ Qus bank/testprep/MISC/01_exam.php
@@ -0,0 +1,27 @@
+<?php
+// $str = "The cat sat on the roof of their house";
+
+// $matches = preg_split("/(the)/i", $str, -1, PREG_SPLIT_DELIM_CAPTURE);
+
+// Array
+// (
+//     [0] => 
+//     [1] => The
+//     [2] =>  cat sat on 
+//     [3] => the
+//     [4] =>  roof of 
+//     [5] => the
+//     [6] => ir house
+// )
+
+// $matches = preg_split("/(the)/i", $str, -1);
+
+// Array
+// (
+//     [0] => 
+//     [1] =>  cat sat on 
+//     [2] =>  roof of 
+//     [3] => ir house
+// )
+
+print_r($matches);
