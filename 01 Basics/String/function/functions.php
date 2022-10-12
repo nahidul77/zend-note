fa4d6f1bc5a722e12bd0075612479a682541ee16
nahidul77 <nahidul.mee@gmail.com>
2022-10-12 15:54:53 +0600

    zend string function strspn added

---
+++ 01 Basics/String/function/functions.php
@@ -0,0 +1,19 @@
+<?php
+
+#__________strspn
+// echo strspn("42 is the answer to the 128th question.", "1234567890");
+//2
+
+// subject does not start with any characters from mask
+// var_dump(strspn("foo", "o"));
+
+// examine two characters from subject starting at offset 1
+// var_dump(strspn("foo", "o", 1, 2));
+
+// examine one character from subject starting at offset 1
+// var_dump(strspn("foo", "o", 1, 1));
+
+
+// int(0)
+// int(2)
+// int(1)
