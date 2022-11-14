e8af12a50bee087eab6955d72b277d6b63098c39
nahidul77 <nahidul.mee@gmail.com>
2022-11-14 12:16:29 +0600

    line jump curly added

--- Job/zainiklab/number_line_jumps.php
+++ Job/zainiklab/number_line_jumps.php
@@ -3,11 +3,13 @@
 function number_line_jumps(int $x1, int $v1, int $x2, int $v2): string
 {
 
-    if ($v1 <= $v2)
+    if ($v1 <= $v2) {
         return "NO";
+    }
 
-    if (($x2 - $x1) % ($v1 - $v2) === 0)
+    if (($x2 - $x1) % ($v1 - $v2) === 0) {
         return "YES";
+    }
 }
 
 
